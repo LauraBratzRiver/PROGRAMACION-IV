dc0276012be03cb5fe1731dcb1498fe19e9c5dc9
LauraBratzRiver <68935174+LauraBratzRiver@users.noreply.github.com>
2022-02-11 22:36:51 -0600

    seguimiento

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ var app = new Vue({
             let sql = '',
                 parametros = [];
             if(this.alumno.accion == 'nuevo'){
-                sql = 'INSERT INTO alumnos (codigo, nombre, direccion, telefono, municipio, departamento, fechadenacimiento, sexo) VALUES (?,?,?,?,?)';
+                sql = 'INSERT INTO alumnos (codigo, nombre, direccion, telefono, municipio, departamento, fechadenacimiento, sexo) VALUES (?,?,?,?,?,?,?,?)';
                 parametros = [this.alumno.codigo,this.alumno.nombre,this.alumno.direccion,this.alumno.telefono,this.alumno.municipio,this.alumno.departamento,this.alumno.fechadenacimiento,this.alumno.sexo];
             }else if(this.alumno.accion == 'modificar'){
                 sql = 'UPDATE alumnos SET codigo=?, nombre=?, direccion=?, telefono=?, municipio=?, departamento=?, fechadenacimiento=?, sexo=? WHERE idAlumno=?';
@@ -69,7 +69,7 @@ var app = new Vue({
         },
         obtenerAlumno(busqueda=''){
             db_sistema.transaction(tx=>{
-                tx.executeSql(`SELECT * FROM alumnos WHERE nombre like "%${busqueda}%" OR codigo like "%${busqueda}%"`, [], (tx, results)=>{
+                tx.executeSql(`SELECT * FROM alumnos WHERE alumno like "%${busqueda}%" OR codigo like "%${busqueda}%"`, [], (tx, results)=>{
                     this.clientes = results.rows;
                     /*this.alumnos = [];
                     for(let i=0; i<results.rows.length; i++){
@@ -87,13 +87,13 @@ var app = new Vue({
             this.alumno.telefono = '';
             this.alumno.municipio = '';
             this.alumno.departamento = '';
-            this.alumno.sexo = '';
+            this.alumno.sexo = ''
         }
     },
     created(){
         db_sistema.transaction(tx=>{
             tx.executeSql('CREATE TABLE IF NOT EXISTS alumnos(idAlumno INTEGER PRIMARY KEY AUTOINCREMENT, '+
-                'codigo char(10), nombre char(75), direccion TEXT, telefono char(10), municipio TEXT, departamento TEXT, fechadenacimiento date)');
+                'codigo char(10), nombre char(75), direccion TEXT, telefono char(10), municipio char(75), departamento TEXT, fechadenacimiento TEXT)');
         }, err=>{
             console.log('Error al crear la tabla de alumnos', err);
         });
